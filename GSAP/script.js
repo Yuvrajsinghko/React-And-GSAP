dcdcb7134cb21e479b1b4cc0a150a887f9c14d29
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-10-31 00:21:48 +0530

    experimented with basic gsap properties

--- GSAP/script.js
+++ GSAP/script.js
@@ -1,12 +1,60 @@
-gsap.to("#box1",{
+gsap.to("#box1", {
+  x: 1200,
+  duration: 3,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: "lime",
+  borderRadius: "50%",
+  scale: 0.5,
+});
+
+gsap.from("#box2", {
+  x: 1200,
+  duration: 3,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: "lime",
+  borderRadius: "50%",
+  scale: 0.5,
+});
+
+
+gsap.from("h1",{
+    opacity:0,
+    y:30,
+    duration:3,
+    delay:1,
+    stagger:0.3
+
+});
+
+gsap.to("#box",{
     x:1200,
-    duration:2,
-    delay:1
+    duration:1,
+    delay:1,
+    rotate:360,
+    repeat:-1,
+    yoyo:true
 })
 
-gsap.to("#box2",{
-    x:1200,
-    y:500,
-    duration:2,
+gsap.to("#box1",{
+    x:1300,
+    rotate:360,
+    duration:1.5,
     delay:1
-})
+});
+
+gsap.to("#box2",{
+    x:1300,
+    backgroundColor:"yellow",
+    duration:1.5,
+    delay:2.5
+});
+
+gsap.to("#box3",{
+    x:1300,
+    scale:0.5,
+    borderRadius:"50%",
+    duration:1.5,
+    delay:4
+});
