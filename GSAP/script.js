aa8bf62b9f9eeb2046cfa505abf7b15cfa642204
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-10-31 00:49:56 +0530

    experimented with some more Gsap properties

--- GSAP/script.js
+++ GSAP/script.js
@@ -1,60 +1,78 @@
-gsap.to("#box1", {
-  x: 1200,
-  duration: 3,
-  delay: 1,
-  rotate: 360,
-  backgroundColor: "lime",
-  borderRadius: "50%",
-  scale: 0.5,
-});
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 3,
+//   delay: 1,
+//   rotate: 360,
+//   backgroundColor: "lime",
+//   borderRadius: "50%",
+//   scale: 0.5,
+// });
 
-gsap.from("#box2", {
-  x: 1200,
-  duration: 3,
-  delay: 1,
-  rotate: 360,
-  backgroundColor: "lime",
-  borderRadius: "50%",
-  scale: 0.5,
-});
+// gsap.from("#box2", {
+//   x: 1200,
+//   duration: 3,
+//   delay: 1,
+//   rotate: 360,
+//   backgroundColor: "lime",
+//   borderRadius: "50%",
+//   scale: 0.5,
+// });
 
+// gsap.from("h1",{
+//     opacity:0,
+//     y:30,
+//     duration:3,
+//     delay:1,
+//     stagger:0.3
 
-gsap.from("h1",{
+// });
+
+// gsap.to("#box",{
+//     x:1200,
+//     duration:1,
+//     delay:1,
+//     rotate:360,
+//     repeat:-1,
+//     yoyo:true
+// })
+
+// let tl = gsap.timeline();
+// tl.to("#box1", {
+//   x: 1300,
+//   duration: 1.5,
+//   rotation: 360,
+//   delay: 1,
+// });
+
+// tl.to("#box2", {
+//   x: 1300,
+//   duration: 1.5,
+// });
+// tl.to("#box3", {
+//   x: 1300,
+//   duration: 1.5,
+// });
+
+let tl = gsap.timeline();
+
+tl.from("h2",{
+    y:-20,
     opacity:0,
-    y:30,
-    duration:3,
-    delay:1,
-    stagger:0.3
-
-});
-
-gsap.to("#box",{
-    x:1200,
     duration:1,
-    delay:1,
-    rotate:360,
-    repeat:-1,
-    yoyo:true
-})
-
-gsap.to("#box1",{
-    x:1300,
-    rotate:360,
-    duration:1.5,
-    delay:1
+    delay:0.5
 });
 
-gsap.to("#box2",{
-    x:1300,
-    backgroundColor:"yellow",
-    duration:1.5,
-    delay:2.5
+tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    stagger:0.3
 });
 
-gsap.to("#box3",{
-    x:1300,
-    scale:0.5,
-    borderRadius:"50%",
-    duration:1.5,
-    delay:4
+tl.from("h1",{
+    y:20,
+    opacity:0,
+    duration:1
+
 });
+
