af9e930506d4cbcf5ad893714fd7a5f8a80f9976
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-05 18:57:57 +0530

    final commit updated with complete code

--- GSAP-Timeline-animation/script.js
+++ GSAP-Timeline-animation/script.js
@@ -1,5 +1,27 @@
+let menu = document.querySelector("#nav i");
+let cross = document.querySelector("#full i");
 let vl = gsap.timeline();
 
-vl.to("#full",{
-    right:1
+vl.to("#full", {
+  right: 0,
+  duration: 0.6,
+});
+
+vl.from("#full h4", {
+  x: 150,
+  duration: 0.7,
+  stagger: 0.3,
+  opacity: 0,
+});
+
+vl.from("#full i", {
+  opacity: 0,
+});
+vl.pause()
+
+menu.addEventListener("click",function(){
+    vl.play();
+});
+cross.addEventListener("click",function(){
+    vl.reverse()
 });
