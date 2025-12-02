b529669714b316292f26861624657f445da3642a
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-02 18:51:44 +0530

    added required assests fonts images

--- mac-book-gsap/src/App.jsx
+++ mac-book-gsap/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import NavBar from './component/NavBar'
+  
+
 
 function App() {
   return (
-    <div>
-      <h1 className="h-screen w-screen bg-amber-700 font-black text-4xl">Hello Ragnar</h1>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   )
 }
 
