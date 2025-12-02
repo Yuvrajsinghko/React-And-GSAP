0de9f3d05de1945740151c5fcf5e4b6b5f882a50
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-02 18:20:27 +0530

    added new packages

---
+++ mac-book-gsap/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="h-screen w-screen bg-amber-700 font-black text-4xl">Hello Ragnar</h1>
+    </div>
+  )
+}
+
+export default App
