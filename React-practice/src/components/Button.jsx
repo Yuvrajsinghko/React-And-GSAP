80e2d892967b1ed8220d0512daff7df77542484a
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-02 13:57:36 +0530

    updated button and header

---
+++ React-practice/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+        <button className='btn'>THis is a button</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ React-practice/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1>Hello Raagnar</h1>
+        <div>
+            <button className='btn'>Hii Boom</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
