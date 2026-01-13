feec80dac7193e37e7f3650b2b6c41223d2c7dd6
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-13 23:00:42 +0530

    updated hero.jsx

--- Stark/src/components/Hero.jsx
+++ Stark/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import { Search } from "lucide-react";
+import HeroCard from "./HeroCard";
 
 const Hero = () => {
   return (
-    <div className="pt-35 min-h-screen w-full flex px-50 gap-8 justify-between ">
+    <div className="pt-35 min-h-screen w-full flex px-50 gap-8 justify-between">
       <div className="hero-left w-[25%] flex flex-col gap-4 px-6 py-6 ">
         <h3 className="text-xl">Search Library</h3>
         <div className="relative w-full">
@@ -36,11 +37,22 @@ const Hero = () => {
         <div className="bg-[#F3F2FC] min-h-auto px-6 py-7 flex flex-col gap-7">
           <h2 className="text-3xl font-bold">Contribute to Stark</h2>
           <p className="text-xl">Got any accessibility resources or news?</p>
-          <button className="bg-[#381fd1] rounded-md px-2 py-3 text-[2vh] text-[#ffff]">Submit a Resource</button>
+          <button className="bg-[#381fd1] rounded-md px-2 py-3 text-[2vh] hover:scale-98 text-[#ffff]">
+            Submit a Resource
+          </button>
         </div>
       </div>
-      <div className="hero-right w-[75%] bg-amber-950">
-        <h2>Hehe</h2>
+      <div className="hero-right w-[75%] bg-amber-500 p-4 ">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="font-extrabold text-6xl mb-3">New in the Library</h1>
+            <p className="text-2xl font-semibold">Newly added accessibility resources, guides, and more.</p>
+          </div>
+          <a href="#" className="underline text-xl text-[#381fd1]">VIEW ALL</a>
+        </div>
+        <div>
+          <HeroCard />
+        </div>
       </div>
     </div>
   );
